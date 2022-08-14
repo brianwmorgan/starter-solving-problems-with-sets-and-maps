69faba707bb3e6ff0bda4ef316de5fe0d1ae78ad
Brian Morgan <brianwmorgan317@gmail.com>
2022-08-14 10:19:00 -0400

    added solution code to each problem as per instructions in lesson 47.4

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,38 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+  const mapA = new Map();
+  let setB = new Set();
+
+  a.forEach((e) => {
+    mapA.set(e, e);
+  });
+
+  b.forEach((e) => {
+    const matchingValue = mapA.get(e);
+    if (matchingValue) {
+      setB.add(e);
+    }
+  });
+
+  const result = [...setB];
+  return result;
+}
+
+// another version of the above:
+
+// function intersection(a, b) {
+//   const map = new Map();
+//   const results = new Set();
+//   for (let e of a) {
+//     map.set(e, e);
+//   }
+
+//   for (let e of b) {
+//     if (map.has(e)) {
+//       results.add(e);
+//     }
+//   }
+
+//   return Array.from(results);
+// }
 
 module.exports = intersection;
